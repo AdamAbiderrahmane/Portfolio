dceaac5efdcda52c0d1b110bc58b3b8dbdf3e062
Adam ABIDERRAHMANE <adam.abiderrahmane@etu.univ-st-etienne.fr>
2025-12-28 21:03:03 +0100

    Cahier des charges réussi à 100% formulaire sans base de données réussi et téléchargement CV en docx et pdf réussi cétait ce qu'il me manquait

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,16 +3,51 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 final class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    #[Route('/blog', name: 'app_blog', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
     {
-        return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
-        ]);
+        if ($request->isMethod('POST')) {
+            $format = $request->request->get('format');
+            $nom = $request->request->get('nom');
+
+            if (!empty($nom)) {
+                $cvPath = $this->getParameter('kernel.project_dir') . '/public/cv/';
+                $fileName = ($format === 'docx') ? 'CV_ABIDERRAHMANE_ADAM.docx' : 'CV_ABIDERRAHMANE_ADAM.pdf';
+                $filePath = $cvPath . $fileName;
+
+                // Vérifie si le fichier existe au sein du dossier CV
+                if (!file_exists($filePath)) {
+                    return new Response("Erreur : Le fichier est introuvable dans " . $cvPath, 404);
+                }
+
+                // Réponse avec fichier
+                $response = new BinaryFileResponse($filePath);
+
+                // Force le type de fichier donc docx et pdf
+                $mimeType = ($format === 'docx') 
+                    ? 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' 
+                    : 'application/pdf';
+                
+                $response->headers->set('Content-Type', $mimeType);
+
+                // Force le téléchargement (parce que j'arrivais pas à faire en sorte que je puisse télécharger mon document donc j'ai forcé volontairement le téléchargement)
+                $response->setContentDisposition(
+                    ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+                    $fileName
+                );
+
+                return $response;
+            }
+        }
+
+        return $this->render('blog/index.html.twig');
     }
 }
